294112dfafa966c537b9a5273a7763517ab5a76d
chirchir <chirchir7370@gmail.com>
2022-08-30 20:09:41 +0300

    add core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
+import { UsersModule } from './core/users/users.module';
+import { AuthModule } from './core/auth/auth.module';
+import { LoggingModule } from './core/logging/logging.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UsersModule, AuthModule, LoggingModule],
   controllers: [AppController],
   providers: [AppService],
 })
