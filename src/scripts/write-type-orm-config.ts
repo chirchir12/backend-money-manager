025ae7b349682104827838840d676631978ff403
chirchir <chirchir7370@gmail.com>
2022-09-26 19:56:14 +0300

    remove console

--- src/scripts/write-type-orm-config.ts
+++ src/scripts/write-type-orm-config.ts
@@ -3,7 +3,6 @@ import * as dotenv from 'dotenv';
 import databaseConfig from '../config/database.config';
 
 dotenv.config();
-console.log(process.env.NODE_ENV);
 fs.writeFileSync(
   'ormconfig.json',
   JSON.stringify(databaseConfig(process.env.NODE_ENV || 'dev'), null, 2),
