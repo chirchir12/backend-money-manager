2d4a3f130c0ef99aa9c3d0864e0f9789db81e47b
chirchir <chirchir7370@gmail.com>
2022-10-15 10:50:33 +0300

    user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -10,24 +10,34 @@ import {
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column({ type: 'boolean', default: true })
   isActive: boolean;
 
   @Column({ type: 'boolean', default: false })
   isArchived: boolean;
 
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  salt: string;
+
+  @Column()
+  hash: string; // passpword
+
   @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-  createDateTime: Date;
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 
   @Column({ type: 'varchar', length: 300 })
   createdBy: string;
-
-  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-  lastChangedDateTime: Date;
-
-  @Column({ type: 'varchar', length: 300 })
-  lastChangedBy: string;
-
-  @Column({ type: 'varchar', length: 300, nullable: true })
-  internalComment: string | null;
 }
