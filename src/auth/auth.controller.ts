cd003b944140386b838c0557ae4605930c2910d6
chirchir <chirchir7370@gmail.com>
2022-10-21 10:44:21 +0300

    controller to sign and log user in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RegisterDto } from '../users/dtos';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local')) // delegate login to the passport module
+  @Post('login')
+  async login(@Request() req) {
+    return await this.authService.login(req.user);
+  }
+
+  @Post('signup')
+  async signup(@Body() user: RegisterDto) {
+    return await this.authService.create(user);
+  }
+}
