af5cb7c225e78be653aa47d7c5c9355df6997a63
chirchir <chirchir7370@gmail.com>
2022-10-24 07:26:53 +0300

    expense dto

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,1 +1,18 @@
-export class CreateExpenseDto {}
+import { IsDateString, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateExpenseDto {
+  @IsNumber()
+  @IsNotEmpty()
+  readonly category: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly amount: number;
+
+  @IsNotEmpty()
+  readonly comment: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  readonly date: Date;
+}
