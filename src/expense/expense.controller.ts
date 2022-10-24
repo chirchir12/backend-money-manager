c6a29ab57dd6862ee6aae14d95bbb6e945bb3371
chirchir <chirchir7370@gmail.com>
2022-10-24 08:14:41 +0300

    expense controller

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -6,23 +6,28 @@ import {
   Patch,
   Param,
   Delete,
+  Request,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('expense')
 export class ExpenseController {
   constructor(private readonly expenseService: ExpenseService) {}
 
   @Post()
-  create(@Body() createExpenseDto: CreateExpenseDto) {
-    return this.expenseService.create(createExpenseDto);
+  create(@Body() createExpenseDto: CreateExpenseDto, @Request() req: any) {
+    return this.expenseService.create(createExpenseDto, req.user.id);
   }
 
   @Get()
-  findAll() {
-    return this.expenseService.findAll();
+  findAll(@Query() query) {
+    return this.expenseService.findAll(query);
   }
 
   @Get(':id')
@@ -33,10 +38,5 @@ export class ExpenseController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateExpenseDto: UpdateExpenseDto) {
     return this.expenseService.update(+id, updateExpenseDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.expenseService.remove(+id);
   }
 }
