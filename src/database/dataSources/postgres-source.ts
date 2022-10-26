d8bda4ae498079c05643861ba1edb50f0c412ff9
chirchir <chirchir7370@gmail.com>
2022-10-26 20:08:38 +0300

    income seeds

--- src/database/dataSources/postgres-source.ts
+++ src/database/dataSources/postgres-source.ts
@@ -1,10 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { validateDBConfig } from '../validations';
-import { Income } from '../../income/entities/income.entity';
-import { User } from '../../users/entities/user.entity';
-import { Expense } from '../../expense/entities/expense.entity';
-import { Category } from '../../shared/categories/entities/category.entity';
 
 dotenv.config();
 
@@ -24,7 +20,7 @@ export const DbConnection: DataSourceOptions = {
   password: envVars.DB_PASS,
   host: envVars.DB_HOST,
   port: envVars.DB_PORT,
-  entities: [Income, User, Expense, Category],
+  entities: ['dist/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
 };
 
